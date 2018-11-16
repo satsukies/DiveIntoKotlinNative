470a81b19e40658556cc626f8162c73efbc18abc
satsukies <satsukies@gmail.com>
2018-11-16 21:18:08 +0900

    implement android app: add id in layout, set values to TextView

--- app/src/main/java/net/ddns/satsukies/diveintokotlinnative/MainActivity.kt
+++ app/src/main/java/net/ddns/satsukies/diveintokotlinnative/MainActivity.kt
@@ -2,11 +2,17 @@ package net.ddns.satsukies.diveintokotlinnative
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import net.ddns.satsukies.diveintokotlinnative.common.createApplicationScreenMessage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.text = createApplicationScreenMessage()
+        textView.textSize = 24f
     }
 }
