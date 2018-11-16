a03ee32273a85cf701c1c7cac7882ae09403bda2
satsukies <satsukies@gmail.com>
2018-11-16 21:33:23 +0900

    generate Xcode project

---
+++ diveintokotlinnative/diveintokotlinnative/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diveintokotlinnative
+//
+//  Created by Satoshi Takeda on 2018/11/16.
+//  Copyright © 2018年 satsukies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ diveintokotlinnative/diveintokotlinnativeTests/diveintokotlinnativeTests.swift
@@ -0,0 +1,34 @@
+//
+//  diveintokotlinnativeTests.swift
+//  diveintokotlinnativeTests
+//
+//  Created by Satoshi Takeda on 2018/11/16.
+//  Copyright © 2018年 satsukies. All rights reserved.
+//
+
+import XCTest
+@testable import diveintokotlinnative
+
+class diveintokotlinnativeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
