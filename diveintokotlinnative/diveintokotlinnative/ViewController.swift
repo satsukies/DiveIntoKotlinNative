1e2acc37785f8b9514cc1ce4cea7a3a8e9d551dd
satsukies <satsukies@gmail.com>
2018-11-17 08:40:09 +0900

    implement iOS app: add UILabel, set values in ViewController.swift

--- diveintokotlinnative/diveintokotlinnative/ViewController.swift
+++ diveintokotlinnative/diveintokotlinnative/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import ShareCode
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.font = label.font.withSize(24)
+        // 共通モジュールに実装したfunctionが呼び出せている
+        label.text = CommonKt.createApplicationScreenMessage()
+        view.addSubview(label)
     }
-
-
 }
 
